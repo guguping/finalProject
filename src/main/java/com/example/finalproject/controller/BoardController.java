8948e31f89cf4a59d8a6eac04eccddd980075710
guguping <qkfhandur@naver.com>
2023-06-27 09:31:46 +0900

    boardMain-html

--- src/main/java/com/example/finalproject/controller/BoardController.java
+++ src/main/java/com/example/finalproject/controller/BoardController.java
@@ -3,9 +3,14 @@ package com.example.finalproject.controller;
 import com.example.finalproject.serivce.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+    @GetMapping("/board/main")
+    public String boardMain() {
+        return "boardPages/boardMain";
+    }
 }
