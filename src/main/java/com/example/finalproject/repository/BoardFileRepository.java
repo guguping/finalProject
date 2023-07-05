d5fd1428cddd2532ce6b20fcb360e0c0f1029b76
juni1015 <jiyoon6293@naver.com>
2023-07-05 18:01:08 +0900

    myPage_detail

--- src/main/java/com/example/finalproject/repository/BoardFileRepository.java
+++ src/main/java/com/example/finalproject/repository/BoardFileRepository.java
@@ -1,9 +1,13 @@
 package com.example.finalproject.repository;
 
+import com.example.finalproject.entitiy.BoardEntity;
 import com.example.finalproject.entitiy.BoardFileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 public interface BoardFileRepository extends JpaRepository<BoardFileEntity, Long> {
+    List<BoardFileEntity> findByBoardEntity(BoardEntity boardEntity);
 }
