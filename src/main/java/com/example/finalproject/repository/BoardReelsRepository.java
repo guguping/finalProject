6df1651b35d82196b958b632d31009ca2834a78e
guguping <qkfhandur@naver.com>
2023-07-12 18:02:04 +0900

    reelsPage-script-good

---
+++ src/main/java/com/example/finalproject/repository/BoardReelsRepository.java
@@ -0,0 +1,7 @@
+package com.example.finalproject.repository;
+
+import com.example.finalproject.entitiy.BoardReelsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardReelsRepository extends JpaRepository<BoardReelsEntity,Long> {
+}
