6c3053dc163f85948d4bb409b2586cdf9b65c56e
guguping <qkfhandur@naver.com>
2023-07-14 19:05:12 +0900

    reelsComment-like

--- src/main/java/com/example/finalproject/dto/LikeDTO.java
+++ src/main/java/com/example/finalproject/dto/LikeDTO.java
@@ -1,6 +1,9 @@
 package com.example.finalproject.dto;
 
 import com.example.finalproject.entitiy.BoardLikeEntity;
+import com.example.finalproject.entitiy.BoardReelsCommentEntity;
+import com.example.finalproject.entitiy.BoardReelsCommentLikeEntity;
+import com.example.finalproject.repository.ReelsCommentLikeRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,19 @@ import lombok.NoArgsConstructor;
 public class LikeDTO {
     private Long id;
     private Long boardId;
+    private Long commentId;
     private Long memberId;
     private int LikeKind;
+
+    public static LikeDTO reelsCommentLikeToDTO(BoardReelsCommentLikeEntity boardReelsCommentLikeEntity) {
+        LikeDTO likeDTO = new LikeDTO();
+        if (boardReelsCommentLikeEntity == null){
+            return null;
+        } else {
+            likeDTO.setId(boardReelsCommentLikeEntity.getId());
+            likeDTO.setMemberId(boardReelsCommentLikeEntity.getMemberEntity().getId());
+            likeDTO.setCommentId(boardReelsCommentLikeEntity.getBoardReelsCommentEntity().getId());
+            return likeDTO;
+        }
+    }
 }
