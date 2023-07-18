ce57122c158170387c5f0272b4ecc492a19aea61
guguping <qkfhandur@naver.com>
2023-07-18 17:46:22 +0900

    Tlqkf

--- src/main/java/com/example/finalproject/controller/ReelsController.java
+++ src/main/java/com/example/finalproject/controller/ReelsController.java
@@ -22,51 +22,63 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ReelsController {
     private final ReelsService reelsService;
+
     @GetMapping("/board/reels")
-    public String reelsPage(Model model , HttpSession session) {
+    public String reelsPage(Model model, HttpSession session) {
         List<BoardReelsDTO> boardReelsDTOList = reelsService.reelsFindAll();
-        List<LikeDTO> boardReelsLikeDTOList = reelsService.findByLike((Long)session.getAttribute("memberId"));
+        List<LikeDTO> boardReelsLikeDTOList = reelsService.findByLike((Long) session.getAttribute("memberId"));
         if (boardReelsDTOList == null) {
-            model.addAttribute("boardReelsList",null);
+            model.addAttribute("boardReelsList", null);
         } else {
-            model.addAttribute("boardReelsList",boardReelsDTOList);
+            model.addAttribute("boardReelsList", boardReelsDTOList);
         }
-        if ( boardReelsLikeDTOList == null) {
-            model.addAttribute("reelsLikeList",null);
+
+        if (boardReelsLikeDTOList == null) {
+            model.addAttribute("reelsLikeList", null);
         } else {
+            int Result = boardReelsDTOList.size() - boardReelsLikeDTOList.size();
+            for (int i = 1; i <= Result; i++) {
+                boardReelsLikeDTOList.add(null);
+            }
             model.addAttribute("reelsLikeList", boardReelsLikeDTOList);
         }
         return "boardPages/boardReels";
     }
+
     @PostMapping("/Reels/comment")
     public ResponseEntity<?> reelsComment(@RequestBody BoardReelsDTO boardReelsDTO,
-                                       Model model, HttpSession httpSession) {
-        Map<String, Object> reelsCommentResponse = reelsService.findByBoardReelsEntityOrderByIdDesc(boardReelsDTO , (Long) httpSession.getAttribute("memberId"));
+                                          Model model, HttpSession httpSession) {
+        Map<String, Object> reelsCommentResponse = reelsService.findByBoardReelsEntityOrderByIdDesc(boardReelsDTO, (Long) httpSession.getAttribute("memberId"));
 
-        return new ResponseEntity<>(reelsCommentResponse , HttpStatus.OK);
+        return new ResponseEntity<>(reelsCommentResponse, HttpStatus.OK);
     }
+
     @PostMapping("/Reels/commentSave")
     public ResponseEntity<?> reelsCommentSave(@RequestBody BoardReelsCommentDTO boardReelsCommentDTO,
-                                           HttpSession session) {
+                                              HttpSession session) {
         boardReelsCommentDTO.setMemberId((Long) session.getAttribute("memberId"));
         reelsService.save(boardReelsCommentDTO);
-            return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
     @PostMapping("/Reels/commentLike")
     public ResponseEntity<?> reelsCommentLike(@RequestBody LikeDTO likeDTO) {
         LikeDTO likeDTOResult = reelsService.saveLike(likeDTO);
         return new ResponseEntity<>(likeDTOResult, HttpStatus.OK);
     }
+
     @PostMapping("/Reels/commentUnLike")
     public ResponseEntity<?> reelsCommnetUnLike(@RequestBody LikeDTO likeDTO) {
         reelsService.deleteLike(likeDTO);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
     @PostMapping("/Reels/likeSave")
     public ResponseEntity<?> reelsLikeSave(@RequestBody LikeDTO likeDTO) {
         LikeDTO likeDTOResult = reelsService.saveReelsLike(likeDTO);
         return new ResponseEntity<>(likeDTOResult, HttpStatus.OK);
     }
+
     @PostMapping("/Reels/likeDelete")
     public ResponseEntity<?> reelsLikeDelete(@RequestBody LikeDTO likeDTO) {
         reelsService.deleteLike(likeDTO);
